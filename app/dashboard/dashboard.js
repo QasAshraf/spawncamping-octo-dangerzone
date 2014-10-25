0b098ee90636edd76977eea6aa508589bdd464f9
Ben Marshall <ben3005@hotmail.com>
2014-10-25 17:36:16 +0100

    Added a marker to MOSI

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -17,30 +17,36 @@ angular.module('myApp.dashboard', ['ngRoute', 'google-maps'.ns()])
     }])
 
     .controller('DashboardCtrl', ['$scope', 'GoogleMapApi'.ns(), function ($scope, GoogleMapApi) {
+        var map = {
+            zoom: 8,
+            bounds: {}
+        };
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function (position) {
                 $scope.$apply(function () {
-                    $scope.map = {
-                        center: {
-                            latitude: (position != 'undefined') ? position.coords.latitude : 53.4667,
-                            longitude: (position != 'undefined') ? position.coords.longitude : 2.2333
-                        },
-                        zoom: 8,
-                        bounds: {}
+                    $scope.map.center = {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
                     };
                 });
             });
         }
         $scope.UserName = "ben3005";
-        $scope.map = {
-            center: {
-                latitude: 53.4667,
-                longitude: 2.2333
-            },
-            zoom: 8,
-            bounds: {}
+        $scope.marker = {
+            idKey: 1,
+            coords: {
+                latitude: 53.476621,
+                longitude: -2.253648
+            }
+        }
+
+        //set a default centre so that if the location is denied it will use defaults
+        map.center = {
+            latitude: 53.4667,
+            longitude: 2.2333
         };
 
+        $scope.map = map;
         GoogleMapApi.then(function (maps) {
             console.log($scope.map)
         });
