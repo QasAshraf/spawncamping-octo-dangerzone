684dcc2630357b5ce42ebd5702853332bc3fce34
Steph Cook <stephc@magmadigital.co.uk>
2014-10-25 13:32:50 +0100

    Fix: Angular

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,9 @@ module.exports = function(grunt) {
                 files: {
                     'jquery.min.js': 'jquery/dist/jquery.min.js',
                     'bootstrap.min.css': 'bootstrap/dist/css/bootstrap.min.css',
-                    'bootstrap.min.js': 'bootstrap/dist/js/bootstrap.min.js'
+                    'bootstrap.min.js': 'bootstrap/dist/js/bootstrap.min.js',
+                    'angular.js': 'angular/angular.js',
+                    'angular-route.js': 'angular-route/angular-route.js'
                 }
             },
 
