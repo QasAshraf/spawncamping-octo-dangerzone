218f4117a78f8f24db0a6c03c6090316cddb7f96
Ben Marshall <ben3005@hotmail.com>
2014-10-26 07:34:13 +0000

    location actually pointing at endpoint

--- app/factories.js
+++ app/factories.js
@@ -1,5 +1,5 @@
 app.factory('groupFactory', ['$http', function ($http) {
-    var url = "/tagchat-api/current/examples/location.json"
+    var url = "/api/location"
     var dataFactory = {};
     dataFactory.getGroups = function () {
         return $http.get(url);
